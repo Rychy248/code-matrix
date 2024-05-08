f6ee8e98778bf9ab4677f591812e5e8ff9801bb5
Rychy248 <jorgeajrhh@gmail.com>
2024-05-08 16:34:59 -0600

    rename

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000
+  server: {
+    port: 3000
   },
-  resolve:{
-    extensions:['.mjs','.js','.jsx','.json']
+  resolve: {
+    extensions: ['.mjs', '.js', '.jsx', '.json']
+  },
+  build: {
+    assetsInlineLimit: 0, // Evita que los archivos se conviertan en datos URI
+    outDir: 'dist', // Directorio de salida para los archivos construidos
+    assetsDir: '.', // Directorio de salida relativo al directorio outDir
+    rollupOptions: {
+      output: {
+        manualChunks: undefined // Deshabilita la agrupación de módulos manuales para evitar problemas con las rutas de los archivos estáticos
+      }
+    }
   }
-})
+});
